d303b8341cb57bcd8d8c3ecee67f5134776a7b0c
fouad ajayi <fouadajayi6@gmail.com>
2024-03-25 12:03:17 +0100

    gettingstarted

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,6 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+
+    private void sum (int a, int b, int c){
+        int result = a + b + c;
     }
 
 }
